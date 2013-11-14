c517a3a92d5a841bfad958c85235bc2738ca39ac
Scott de Jonge <scottdj@bigfish.tv>
2013-11-14 17:06:22 +1000

    Initial Commit.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Turret</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="css/vendor/normalize.min.css">
+        <link rel="stylesheet/css" type="text/css" href="less/styles.css" />
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <script src="js/vendor/less-1.5.0.min.js"></script>
+    </head>
+    <body>
+		<div class="outer">
+			<div class="inner">
+				<div class="navbar">
+				</div>
+			</div>
+		</div>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
