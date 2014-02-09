27a2916b60b7b9dc7ee711f68c6d94bd19a62ad0
Scott de Jonge <dejonge.scott@gmail.com>
2014-02-10 09:57:46 +1000

    Added index.php to root Directory.

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Turret</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width">
+	<script src="../lib/modernizr-2.6.2.min.js"></script>
+	<script src="../lib/less-1.5.0.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="//cloud.typography.com/6120872/680164/css/fonts.css" />
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+
+	<style>
+		body {
+			background: #FF444E;
+			text-align: center;
+		}
+
+		.title {
+			margin: 50px 0px;
+			font-family: 'Ziggurat A', 'Ziggurat B';
+			font-weight: 800;
+			font-style: italic;
+			font-size: 64px;
+			color: #FFFFFF;
+		}
+	</style>
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="column-1">
+				<h1 class="title">turret</h1>
+				<a href="/docs" class="button large dark">Docs</a>
+				<a href="#" class="button large dark">Demo</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
