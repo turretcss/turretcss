997a8c6fa87e837b053f2bf0f94852e95f0bf492
Scott de Jonge <dejonge.scott@gmail.com>
2014-02-10 10:38:50 +1000

    Added Browser Support and Basic Template to Docs

--- docs/index.php
+++ docs/index.php
@@ -13,6 +13,8 @@
 	<div class="side-nav">
 		<ul>
 			<li><a href="#general">General</a></li>
+			<li><a href="#template">Basic Template</a></li>
+			<li><a href="#support">Browser Support</a></li>
 			<li><a href="#media-queries">Media Queries</a></li>
 			<li><a href="#typography">Typography</a></li>
 			<li><a href="#icons">Icons</a></li>
@@ -21,7 +23,6 @@
 </div>
 
 <div class="column-3-4">
-
 
 <h2 id="general" class="section-title">General</h2>
 <!-- Structure -->
@@ -61,6 +62,106 @@
 ~"only screen and (-o-min-device-pixel-ratio: 3/2)",
 ~"only screen and (min-device-pixel-ratio: 1.5)";'); ?>
 </code></pre>
+
+<!-- Basic Template -->
+
+<h2 id="template" class="section-title">Basic Template</h2>
+<p>Turret styles are compiled into <code>main.css</code> in the <code>/css</code> directory. Mobile first responsive styles require the viewport to be <code>width=device-width</code>, <code>initial-scale=1.0</code>, <code>maximum-scale=1.0</code>, and <code>user-scalable=no</code>. Turret components require JQuery. Optional support for IE8 requires <code>HTML5Siv.js</code> and <code>respond.js</code></p>
+<pre><code class="hljs html"><?php echo htmlentities('<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<title>Turret Template</title>
+
+	<!-- Turret -->
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<h1>Hello, world!</h1>
+
+	<!-- JQuery -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write(\'<script src="../lib/jquery-1.10.1.min.js"><\/script>\')</script>
+
+	<!-- Components -->
+	<script src="../js/jquery.easing.js"></script>
+	<script src="../js/jquery.scrolltop.js"></script>
+	<script src="../js/jquery.gmap.js"></script>
+	<script src="../js/jquery.slides.js"></script>
+</body>
+</html>'); ?></code></pre>
+
+<!-- Browser Support -->
+
+<h2 id="support" class="section-title">Browser Support</h2>
+<h3 class="section-sub-title">CSS</h2>
+<p>Some CSS3 properties and HTML5 elements are not fully supported by Internet Explorer 8 and 9. In addition, Internet Explorer 8 requires the use of <a href="https://github.com/scottjehl/Respond"><code>Respond.js</code></a> to enable media query support.</p>
+<table class="table table-bordered table-block table-responsive">
+	<thead>
+		<tr>
+			<th>Feature</th>
+			<th>IE8</th>
+			<th>IE9</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><code>border-radius</code></td>
+			<td class="is-unsupported">Not supported</td>
+			<td class="is-supported">Supported</td>
+		</tr>
+		<tr>
+			<td><code>box-shadow</code></td>
+			<td class="is-unsupported">Not supported</td>
+			<td class="is-supported">Supported</td>
+		</tr>
+		<tr>
+			<td><code>transform</code></td>
+			<td class="is-unsupported">Not supported</td>
+			<td class="is-supported">Supported, with <code>-ms</code> prefix</td>
+		</tr>
+		<tr>
+			<td><code>transition</code></td>
+			<td class="is-unsupported">Not supported</td>
+			<td class="is-unsupported">Not supported</td>
+		</tr>
+		<tr>
+			<td><code>placeholder</code></td>
+			<td class="is-unsupported">Not supported</td>
+			<td class="is-unsupported">Not supported</td>
+		</tr>
+	</tbody>
+</table>
+<p>Visit <a href="http://caniuse.com/">Can I use...</a> for details on browser support of CSS3 and HTML5 features.</p>
+
+<h3 class="section-sub-title">IE8 and box-sizing</h2>
+<p>IE8 does not fully support <code>box-sizing: border-box;</code> when combined with <code>min-width</code>, <code>max-width</code>, <code>min-height</code>, or <code>max-height</code>. Cannot use max-width on <code>.container</code>.</p>
+
+<h3 class="section-sub-title">Internet Explorer 8 and Respond.js</h3>
+<p>Beware of the following caveats when using Respond.js in your development and production environments for Internet Explorer 8.</p>
+
+<h4 class="section-sub-title">Respond.js and cross-domain CSS</h4>
+<p>Using Respond.js with CSS hosted on a different (sub)domain (for example, on a CDN) requires some additional setup. See the <a href="https://github.com/scottjehl/Respond"><code>Respond.js</code></a> docs for details.</p>
+
+<h4 class="section-sub-title">Respond.js and <code>file://</code></h4>
+<p>Due to browser security rules, Respond.js doesn't work with pages viewed via the <code>file://</code> protocol (like when opening a local HTML file). To test responsive features in IE8, view your pages over HTTP(S). See the <a href="https://github.com/scottjehl/Respond"><code>Respond.js</code></a> docs for details.</p>
+
+<h4 class="section-sub-title">Respond.js and <code>@import</code></h4>
+<p>Respond.js doesn't work with CSS that's referenced via <code>@import</code>. In particular, some Drupal configurations are known to use <code>@import</code>. See the <a href="https://github.com/scottjehl/Respond"><code>Respond.js</code></a> docs for details.</p>
+
+<h3 class="section-sub-title">IE Compatibility modes</h2>
+<p>Bootstrap is not supported in the old Internet Explorer compatibility modes. To be sure you're using the latest rendering mode for IE, consider including the appropriate <code><?php echo htmlentities('<meta>'); ?></code> tag in your pages:</p>
+
+<pre><code class="hljs html"><?php echo htmlentities('<meta http-equiv="X-UA-Compatible" content="IE=edge">'); ?></code></pre>
+
 
 <!-- Typography -->
 
