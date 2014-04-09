0b07574b099df71de119dce0a22ddb438fea5011
Scott de Jonge <dejonge.scott@gmail.com>
2014-04-09 12:32:01 +1000

    Icons and Styles

--- docs/icons.php
+++ docs/icons.php
@@ -13,62 +13,42 @@
 
 <div class="column-1">
 
+<div class="icons clearfix">
+	<?php include('includes/icons.php'); ?>
+</div>
+
 <!-- Icons -->
 <div class="panel">
 	<h2 id="icons" class="section-title">Icons</h2>
-	<div class="row">
-		<div class="icon"><span class="icon-volume-mute"></span></div>
-		<div class="icon"><span class="icon-volume-med"></span></div>
-		<div class="icon"><span class="icon-volume-low"></span></div>
-		<div class="icon"><span class="icon-volume-high"></span></div>
-		<div class="icon"><span class="icon-twitter"></span></div>
-		<div class="icon"><span class="icon-subtitles"></span></div>
-		<div class="icon"><span class="icon-search"></span></div>
-		<div class="icon"><span class="icon-refresh"></span></div>
-		<div class="icon"><span class="icon-question"></span></div>
-		<div class="icon"><span class="icon-print"></span></div>
-		<div class="icon"><span class="icon-play"></span></div>
-		<div class="icon"><span class="icon-play-alt"></span></div>
-		<div class="icon"><span class="icon-pinterest"></span></div>
-		<div class="icon"><span class="icon-people"></span></div>
-		<div class="icon"><span class="icon-pause"></span></div>
-		<div class="icon"><span class="icon-map"></span></div>
-		<div class="icon"><span class="icon-lock"></span></div>
-		<div class="icon"><span class="icon-loading"></span></div>
-		<div class="icon"><span class="icon-innovation"></span></div>
-		<div class="icon"><span class="icon-fullscreen"></span></div>
-		<div class="icon"><span class="icon-fullscreen-close"></span></div>
-		<div class="icon"><span class="icon-environment"></span></div>
-		<div class="icon"><span class="icon-community"></span></div>
-		<div class="icon"><span class="icon-close"></span></div>
-		<div class="icon"><span class="icon-clock"></span></div>
-		<div class="icon"><span class="icon-circle"></span></div>
-		<div class="icon"><span class="icon-circle-full"></span></div>
-		<div class="icon"><span class="icon-captions"></span></div>
-		<div class="icon"><span class="icon-angle-down"></span></div>
-		<div class="icon"><span class="icon-angle-left"></span></div>
-		<div class="icon"><span class="icon-angle-right"></span></div>
-		<div class="icon"><span class="icon-angle-up"></span></div>
-		<div class="icon"><span class="icon-bookmark-alt"></span></div>
-		<div class="icon"><span class="icon-bookmark"></span></div>
-		<div class="icon"><span class="icon-calendar"></span></div>
-		<div class="icon"><span class="icon-linkedin"></span></div>
-		<div class="icon"><span class="icon-facebook"></span></div>
-		<div class="icon"><span class="icon-instagram"></span></div>
-		<div class="icon"><span class="icon-share"></span></div>
-		<div class="icon"><span class="icon-youtube-alt"></span></div>
-		<div class="icon"><span class="icon-youtube"></span></div>
-		<div class="icon"><span class="icon-tick"></span></div>
-		<div class="icon"><span class="icon-minus"></span></div>
-		<div class="icon"><span class="icon-plus"></span></div>
-		<div class="icon"><span class="icon-grid"></span></div>
-		<div class="icon"><span class="icon-list"></span></div>
-		<div class="icon"><span class="icon-google-plus"></span></div>
-		<div class="icon"><span class="icon-email"></span></div>
-	</div>
-</div>
 
-<pre><code class="hljs html"><?php echo htmlentities('<span class="ICON-NAME"></span>'); ?></code></pre>
+	<!-- Inline Icons -->
+	<h3 class="section-sub-title">Inline Icons</h3>
+	<p>Place Icons inline using a <code><span></code> tag.</p>
+	<p><span class="icon-play"></span> icon play</p>
+	<pre><code class="hljs html"><?php echo htmlentities('<span class="icon-play"></span> icon play'); ?></code></pre>
+
+	<!-- List Icons -->
+	<h3 class="section-sub-title">List Icons</h3>
+	<p>Use <code>icon-ul</code> and <code>icon-li</code> to replace default bullets in unordered lists.</p>
+	<ul class="icon-ul">
+		<li><i class="icon-li icon-arrow-right"></i>List Item</li>
+		<li><i class="icon-li icon-arrow-right"></i>List Item</li>
+		<li><i class="icon-li icon-arrow-right"></i>List Item</li>
+		<li><i class="icon-li icon-arrow-right"></i>List Item</li>
+	</ul>
+	<pre><code class="hljs html"><?php echo htmlentities('<ul class="icon-ul">
+	<li><i class="icon-li icon-arrow-right"></i>List Item</li>
+	<li><i class="icon-li icon-arrow-right"></i>List Item</li>
+	<li><i class="icon-li icon-arrow-right"></i>List Item</li>
+	<li><i class="icon-li icon-arrow-right"></i>List Item</li>
+</ul>'); ?></code></pre>
+
+	<!-- Spinning Icons -->
+	<h3 class="section-sub-title">Spinning Icons</h3>
+	<p>Use the <code>icon-spin</code> class to get any icon to rotate.</p>
+	<p><span class="icon-spin icon-loading"></span></p>
+	<pre><code class="hljs html"><?php echo htmlentities('<span class="icon-spin icon-loading"></span>'); ?></code></pre>
+</div>
 
 </div>
 
