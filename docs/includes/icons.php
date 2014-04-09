ac45d8cce23836a4fe5019d54b8e66aced5175e3
Scott de Jonge <dejonge.scott@gmail.com>
2014-04-09 12:31:15 +1000

    Added new Animations Files.

---
+++ docs/includes/icons.php
@@ -0,0 +1,287 @@
+<div class="icon">
+	<span class="icon-youtube"></span><span class="icon-name"> icon-youtube</span>
+</div>
+
+<div class="icon">
+	<span class="icon-youtube-alt"></span><span class="icon-name"> icon-youtube-alt</span>
+</div>
+
+<div class="icon">
+	<span class="icon-volume-mute"></span><span class="icon-name"> icon-volume-mute</span>
+</div>
+
+<div class="icon">
+	<span class="icon-volume-med"></span><span class="icon-name"> icon-volume-med</span>
+</div>
+
+<div class="icon">
+	<span class="icon-volume-low"></span><span class="icon-name"> icon-volume-low</span>
+</div>
+
+<div class="icon">
+	<span class="icon-volume-high"></span><span class="icon-name"> icon-volume-high</span>
+</div>
+
+<div class="icon">
+	<span class="icon-twitter"></span><span class="icon-name"> icon-twitter</span>
+</div>
+
+<div class="icon">
+	<span class="icon-tick"></span><span class="icon-name"> icon-tick</span>
+</div>
+
+<div class="icon">
+	<span class="icon-star-thin"></span><span class="icon-name"> icon-star-thin</span>
+</div>
+
+<div class="icon">
+	<span class="icon-star-thin-full"></span><span class="icon-name"> icon-star-thin-full</span>
+</div>
+
+<div class="icon">
+	<span class="icon-single-angle-thin-up"></span><span class="icon-name"> icon-single-angle-thin-up</span>
+</div>
+
+<div class="icon">
+	<span class="icon-single-angle-thin-right"></span><span class="icon-name"> icon-single-angle-thin-right</span>
+</div>
+
+<div class="icon">
+	<span class="icon-single-angle-thin-left"></span><span class="icon-name"> icon-single-angle-thin-left</span>
+</div>
+
+<div class="icon">
+	<span class="icon-single-angle-thin-down"></span><span class="icon-name"> icon-single-angle-thin-down</span>
+</div>
+
+<div class="icon">
+	<span class="icon-share"></span><span class="icon-name"> icon-share</span>
+</div>
+
+<div class="icon">
+	<span class="icon-search"></span><span class="icon-name"> icon-search</span>
+</div>
+
+<div class="icon">
+	<span class="icon-refresh"></span><span class="icon-name"> icon-refresh</span>
+</div>
+
+<div class="icon">
+	<span class="icon-question"></span><span class="icon-name"> icon-question</span>
+</div>
+
+<div class="icon">
+	<span class="icon-print"></span><span class="icon-name"> icon-print</span>
+</div>
+
+<div class="icon">
+	<span class="icon-plus"></span><span class="icon-name"> icon-plus</span>
+</div>
+
+<div class="icon">
+	<span class="icon-plus-thin"></span><span class="icon-name"> icon-plus-thin</span>
+</div>
+
+<div class="icon">
+	<span class="icon-plus-thick"></span><span class="icon-name"> icon-plus-thick</span>
+</div>
+
+<div class="icon">
+	<span class="icon-play"></span><span class="icon-name"> icon-play</span>
+</div>
+
+<div class="icon">
+	<span class="icon-play-alt"></span><span class="icon-name"> icon-play-alt</span>
+</div>
+
+<div class="icon">
+	<span class="icon-pinterest"></span><span class="icon-name"> icon-pinterest</span>
+</div>
+
+<div class="icon">
+	<span class="icon-people"></span><span class="icon-name"> icon-people</span>
+</div>
+
+<div class="icon">
+	<span class="icon-pause"></span><span class="icon-name"> icon-pause</span>
+</div>
+
+<div class="icon">
+	<span class="icon-minus"></span><span class="icon-name"> icon-minus</span>
+</div>
+
+<div class="icon">
+	<span class="icon-minus-thick"></span><span class="icon-name"> icon-minus-thick</span>
+</div>
+
+<div class="icon">
+	<span class="icon-map"></span><span class="icon-name"> icon-map</span>
+</div>
+
+<div class="icon">
+	<span class="icon-lock"></span><span class="icon-name"> icon-lock</span>
+</div>
+
+<div class="icon">
+	<span class="icon-loading"></span><span class="icon-name"> icon-loading</span>
+</div>
+
+<div class="icon">
+	<span class="icon-list"></span><span class="icon-name"> icon-list</span>
+</div>
+
+<div class="icon">
+	<span class="icon-linkedin"></span><span class="icon-name"> icon-linkedin</span>
+</div>
+
+<div class="icon">
+	<span class="icon-instagram"></span><span class="icon-name"> icon-instagram</span>
+</div>
+
+<div class="icon">
+	<span class="icon-info"></span><span class="icon-name"> icon-info</span>
+</div>
+
+<div class="icon">
+	<span class="icon-grid"></span><span class="icon-name"> icon-grid</span>
+</div>
+
+<div class="icon">
+	<span class="icon-fullscreen"></span><span class="icon-name"> icon-fullscreen</span>
+</div>
+
+<div class="icon">
+	<span class="icon-fullscreen-close"></span><span class="icon-name"> icon-fullscreen-close</span>
+</div>
+
+<div class="icon">
+	<span class="icon-facebook"></span><span class="icon-name"> icon-facebook</span>
+</div>
+
+<div class="icon">
+	<span class="icon-edit"></span><span class="icon-name"> icon-edit</span>
+</div>
+
+<div class="icon">
+	<span class="icon-double-angle-thin-up"></span><span class="icon-name"> icon-double-angle-thin-up</span>
+</div>
+
+<div class="icon">
+	<span class="icon-double-angle-thin-right"></span><span class="icon-name"> icon-double-angle-thin-right</span>
+</div>
+
+<div class="icon">
+	<span class="icon-double-angle-thin-left"></span><span class="icon-name"> icon-double-angle-thin-left</span>
+</div>
+
+<div class="icon">
+	<span class="icon-double-angle-thin-down"></span><span class="icon-name"> icon-double-angle-thin-down</span>
+</div>
+
+<div class="icon">
+	<span class="icon-close"></span><span class="icon-name"> icon-close</span>
+</div>
+
+<div class="icon">
+	<span class="icon-close-thin"></span><span class="icon-name"> icon-close-thin</span>
+</div>
+
+<div class="icon">
+	<span class="icon-close-thick"></span><span class="icon-name"> icon-close-thick</span>
+</div>
+
+<div class="icon">
+	<span class="icon-clock"></span><span class="icon-name"> icon-clock</span>
+</div>
+
+<div class="icon">
+	<span class="icon-circle"></span><span class="icon-name"> icon-circle</span>
+</div>
+
+<div class="icon">
+	<span class="icon-circle-full"></span><span class="icon-name"> icon-circle-full</span>
+</div>
+
+<div class="icon">
+	<span class="icon-captions"></span><span class="icon-name"> icon-captions</span>
+</div>
+
+<div class="icon">
+	<span class="icon-camera"></span><span class="icon-name"> icon-camera</span>
+</div>
+
+<div class="icon">
+	<span class="icon-calendar"></span><span class="icon-name"> icon-calendar</span>
+</div>
+
+<div class="icon">
+	<span class="icon-bookmark"></span><span class="icon-name"> icon-bookmark</span>
+</div>
+
+<div class="icon">
+	<span class="icon-bookmark-alt"></span><span class="icon-name"> icon-bookmark-alt</span>
+</div>
+
+<div class="icon">
+	<span class="icon-arrow-up"></span><span class="icon-name"> icon-arrow-up</span>
+</div>
+
+<div class="icon">
+	<span class="icon-arrow-thin-up"></span><span class="icon-name"> icon-arrow-thin-up</span>
+</div>
+
+<div class="icon">
+	<span class="icon-arrow-thin-right"></span><span class="icon-name"> icon-arrow-thin-right</span>
+</div>
+
+<div class="icon">
+	<span class="icon-arrow-thin-left"></span><span class="icon-name"> icon-arrow-thin-left</span>
+</div>
+
+<div class="icon">
+	<span class="icon-arrow-thin-down"></span><span class="icon-name"> icon-arrow-thin-down</span>
+</div>
+
+<div class="icon">
+	<span class="icon-arrow-right"></span><span class="icon-name"> icon-arrow-right</span>
+</div>
+
+<div class="icon">
+	<span class="icon-arrow-left"></span><span class="icon-name"> icon-arrow-left</span>
+</div>
+
+<div class="icon">
+	<span class="icon-arrow-down"></span><span class="icon-name"> icon-arrow-down</span>
+</div>
+
+<div class="icon">
+	<span class="icon-angle-up"></span><span class="icon-name"> icon-angle-up</span>
+</div>
+
+<div class="icon">
+	<span class="icon-angle-thin-up"></span><span class="icon-name"> icon-angle-thin-up</span>
+</div>
+
+<div class="icon">
+	<span class="icon-angle-thin-right"></span><span class="icon-name"> icon-angle-thin-right</span>
+</div>
+
+<div class="icon">
+	<span class="icon-angle-thin-left"></span><span class="icon-name"> icon-angle-thin-left</span>
+</div>
+
+<div class="icon">
+	<span class="icon-angle-thin-down"></span><span class="icon-name"> icon-angle-thin-down</span>
+</div>
+
+<div class="icon">
+	<span class="icon-angle-right"></span><span class="icon-name"> icon-angle-right</span>
+</div>
+
+<div class="icon">
+	<span class="icon-angle-left"></span><span class="icon-name"> icon-angle-left</span>
+</div>
+
+<div class="icon">
+	<span class="icon-angle-down"></span><span class="icon-name"> icon-angle-down</span>
+</div>
