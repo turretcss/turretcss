539eb0bb546a7bddb8350c5e7694ee57d15c0a3a
Scott de Jonge <dejonge.scott@gmail.com>
2014-04-09 15:53:56 +1000

    Screen Resolution Table

--- docs/index.php
+++ docs/index.php
@@ -13,7 +13,7 @@
 <div class="column-1-4">
 	<div class="side-nav">
 		<ul>
-			<li><a href="#general">General</a></li>
+			<li><a href="#resolutions">Screen Resolutions</a></li>
 			<li><a href="#template">Basic Template</a></li>
 			<li><a href="#support">Browser Support</a></li>
 			<li><a href="#media-queries">Media Queries</a></li>
@@ -24,9 +24,78 @@
 
 <div class="column-3-4">
 
+	<!-- Screen Resolutions -->
+	<div class="panel">
+		<h2 id="resolutions" class="section-title">Screen Resolutions</h2>
+		<table class="table table-bordered table-block table-responsive">
+			<thead>
+				<tr>
+					<th>Device</th>
+					<th>Width <small>(px)</small></th>
+					<th>Height <small>(px)</small></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>iPhone 4/iPhone 4s</td>
+					<td>640</td>
+					<td>960</td>
+				</tr>
+				<tr>
+					<td>iPhone 5/iPhone 5s</td>
+					<td>640</td>
+					<td>1136</td>
+				</tr>
+				<tr>
+					<td>iPad 1/iPad 2/iPad Mini</td>
+					<td>768</td>
+					<td>1024</td>
+				</tr>
+				<tr>
+					<td>iPad Retina/iPad Mini Retina</td>
+					<td>1536</td>
+					<td>2048</td>
+				</tr>
+				<tr>
+					<td>MacBook Air 11"</td>
+					<td>1366</td>
+					<td>768</td>
+				</tr>
+				<tr>
+					<td>MacBook Air 13"</td>
+					<td>1440</td>
+					<td>900</td>
+				</tr>
+				<tr>
+					<td>MacBook Pro 13"</td>
+					<td>1280</td>
+					<td>800</td>
+				</tr>
+				<tr>
+					<td>MacBook Pro 13" Retina</td>
+					<td>2560</td>
+					<td>1600</td>
+				</tr>
+				<tr>
+					<td>MacBook Pro 15"</td>
+					<td>1440</td>
+					<td>900</td>
+				</tr>
+				<tr>
+					<td>MacBook Pro 15" High Resolution</td>
+					<td>1680</td>
+					<td>1050</td>
+				</tr>
+				<tr>
+					<td>MacBook Pro 15" Retina</td>
+					<td>2880</td>
+					<td>1800</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 
 </div>
-
 
 </div>
 </div>
