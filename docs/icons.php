f81852b8e78bebe290178109eb004bb11d5c5799
Scott de Jonge <dejonge.scott@gmail.com>
2014-04-09 11:22:34 +1000

    Created Icons Page

---
+++ docs/icons.php
@@ -0,0 +1,75 @@
+<?php include('includes/head.php'); ?>
+
+<div class="page-header" style="height:200px;">
+	<div class="container">
+		<div class="page-header-content column-1">
+			<h1 class="page-title">Icons</h1>
+		</div>
+	</div>
+</div>
+
+<div id="icons" class="content" class="row">
+<div class="container">
+
+<div class="column-1">
+
+<!-- Icons -->
+<div class="panel">
+	<h2 id="icons" class="section-title">Icons</h2>
+	<div class="row">
+		<div class="icon"><span class="icon-volume-mute"></span></div>
+		<div class="icon"><span class="icon-volume-med"></span></div>
+		<div class="icon"><span class="icon-volume-low"></span></div>
+		<div class="icon"><span class="icon-volume-high"></span></div>
+		<div class="icon"><span class="icon-twitter"></span></div>
+		<div class="icon"><span class="icon-subtitles"></span></div>
+		<div class="icon"><span class="icon-search"></span></div>
+		<div class="icon"><span class="icon-refresh"></span></div>
+		<div class="icon"><span class="icon-question"></span></div>
+		<div class="icon"><span class="icon-print"></span></div>
+		<div class="icon"><span class="icon-play"></span></div>
+		<div class="icon"><span class="icon-play-alt"></span></div>
+		<div class="icon"><span class="icon-pinterest"></span></div>
+		<div class="icon"><span class="icon-people"></span></div>
+		<div class="icon"><span class="icon-pause"></span></div>
+		<div class="icon"><span class="icon-map"></span></div>
+		<div class="icon"><span class="icon-lock"></span></div>
+		<div class="icon"><span class="icon-loading"></span></div>
+		<div class="icon"><span class="icon-innovation"></span></div>
+		<div class="icon"><span class="icon-fullscreen"></span></div>
+		<div class="icon"><span class="icon-fullscreen-close"></span></div>
+		<div class="icon"><span class="icon-environment"></span></div>
+		<div class="icon"><span class="icon-community"></span></div>
+		<div class="icon"><span class="icon-close"></span></div>
+		<div class="icon"><span class="icon-clock"></span></div>
+		<div class="icon"><span class="icon-circle"></span></div>
+		<div class="icon"><span class="icon-circle-full"></span></div>
+		<div class="icon"><span class="icon-captions"></span></div>
+		<div class="icon"><span class="icon-angle-down"></span></div>
+		<div class="icon"><span class="icon-angle-left"></span></div>
+		<div class="icon"><span class="icon-angle-right"></span></div>
+		<div class="icon"><span class="icon-angle-up"></span></div>
+		<div class="icon"><span class="icon-bookmark-alt"></span></div>
+		<div class="icon"><span class="icon-bookmark"></span></div>
+		<div class="icon"><span class="icon-calendar"></span></div>
+		<div class="icon"><span class="icon-linkedin"></span></div>
+		<div class="icon"><span class="icon-facebook"></span></div>
+		<div class="icon"><span class="icon-instagram"></span></div>
+		<div class="icon"><span class="icon-share"></span></div>
+		<div class="icon"><span class="icon-youtube-alt"></span></div>
+		<div class="icon"><span class="icon-youtube"></span></div>
+		<div class="icon"><span class="icon-tick"></span></div>
+		<div class="icon"><span class="icon-minus"></span></div>
+		<div class="icon"><span class="icon-plus"></span></div>
+		<div class="icon"><span class="icon-grid"></span></div>
+		<div class="icon"><span class="icon-list"></span></div>
+		<div class="icon"><span class="icon-google-plus"></span></div>
+		<div class="icon"><span class="icon-email"></span></div>
+	</div>
+</div>
+
+<pre><code class="hljs html"><?php echo htmlentities('<span class="ICON-NAME"></span>'); ?></code></pre>
+
+</div>
+
+<?php include('includes/footer.php'); ?>
