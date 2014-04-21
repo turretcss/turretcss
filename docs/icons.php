cdf2c4a6413a4242175453caedc65d4ed971bf5b
Scott de Jonge <dejonge.scott@gmail.com>
2014-04-22 09:47:48 +1000

    Icon Docs Fixes. New Page Header description.

--- docs/icons.php
+++ docs/icons.php
@@ -1,9 +1,12 @@
 <?php include('includes/head.php'); ?>
 
-<div class="page-header" style="height:200px;">
+<div class="page-header">
 	<div class="container">
 		<div class="page-header-content column-1">
-			<h1 class="page-title">Icons</h1>
+			<div class="page-header-content-inner">
+				<h1 class="page-title">Icons</h1>
+				<p class="page-description">An outline of the SVG Icon Font library with utility classes for animations, lists, and icon display types.</p>
+			</div>
 		</div>
 	</div>
 </div>
@@ -13,17 +16,21 @@
 
 <div class="column-1">
 
-<div class="icons clearfix">
-	<?php include('includes/icons.php'); ?>
+<!-- Icons -->
+<div class="panel">
+	<h2 id="icons" class="section-title">Icons</h2>
+	<div class="row">
+		<?php include('includes/icons.php'); ?>
+	</div>
 </div>
 
 <!-- Icons -->
 <div class="panel">
-	<h2 id="icons" class="section-title">Icons</h2>
+	<h2 id="icons" class="section-title">Icon Utilites</h2>
 
 	<!-- Inline Icons -->
 	<h3 class="section-sub-title">Inline Icons</h3>
-	<p>Place Icons inline using a <code><span></code> tag.</p>
+	<p>Place Icons inline using a <code><?php echo htmlentities('<span>'); ?></code> tag.</p>
 	<p><span class="icon-play"></span> icon play</p>
 	<pre><code class="hljs html"><?php echo htmlentities('<span class="icon-play"></span> icon play'); ?></code></pre>
 
@@ -48,7 +55,7 @@
 	<p>Use the <code>icon-spin</code> class to get any icon to rotate.</p>
 	<p><span class="icon-spin icon-loading"></span></p>
 	<pre><code class="hljs html"><?php echo htmlentities('<span class="icon-spin icon-loading"></span>'); ?></code></pre>
-	<div class="alert alert-info"> CSS3 animations aren't supported in IE8 - IE9.</div>
+	<div class="alert alert-info"> CSS3 animations aren't supported in IE8 - IE9.<button type="button" class="alert-close" data-dismiss></button></div>
 </div>
 
 </div>
