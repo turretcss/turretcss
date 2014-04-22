d6a2e9fcde23994c2455c4c3ac918ff9ab2777ee
Scott de Jonge <dejonge.scott@gmail.com>
2014-04-22 10:05:17 +1000

    Scroll Top Removed Text and Footer Copyright Center

--- docs/includes/footer.php
+++ docs/includes/footer.php
@@ -1,12 +1,12 @@
 </div>
-<div class="scroll-top fixed right" data-scroll-top>Top</div>
+<div class="scroll-top fixed right" data-scroll-top></div>
 <div class="clearfix"></div>
 </div>
 <footer id="footer">
 	<div class="container">
 		<div class="row">
 			<div class="footer-content column-1">
-				<p class="footer-copyright right">&copy; <?php echo date('Y'); ?> Bigfish.tv</p>
+				<p class="footer-copyright">&copy; <?php echo date('Y'); ?> Bigfish.tv</p>
 			</div>
 		</div>
 	</div>
