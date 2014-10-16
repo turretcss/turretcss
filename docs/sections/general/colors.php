200beb7e21029d77ad6a73e721cf6be0ca665672
Scott de Jonge <dejonge.scott@gmail.com>
2014-10-16 15:58:11 +1000

    Refactoring Utility Classes. Moved Gutter Utilities to grid.less. Text and Background Color Docs

--- docs/sections/general/colors.php
+++ docs/sections/general/colors.php
@@ -3,23 +3,23 @@
 	<h3 class="section-sub-title">Shades</h3>
 	<div class="row">
 		<div class="color column-1-6">
-			<div class="swatch white"></div>
+			<div class="swatch background-white"></div>
 			<small>White</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch light-grey"></div>
+			<div class="swatch background-light-grey"></div>
 			<small>Light Grey</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch grey"></div>
+			<div class="swatch background-grey"></div>
 			<small>Grey</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch dark-grey"></div>
+			<div class="swatch background-dark-grey"></div>
 			<small>Dark Grey</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch black"></div>
+			<div class="swatch background-black"></div>
 			<small>Black</small>
 		</div>
 	</div>
@@ -28,19 +28,19 @@
 	<h3 class="section-sub-title">Indicators</h3>
 	<div class="row">
 		<div class="color column-1-6">
-			<div class="swatch error"></div>
+			<div class="swatch background-error"></div>
 			<small>Error</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch warning"></div>
+			<div class="swatch background-warning"></div>
 			<small>Warning</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch success"></div>
+			<div class="swatch background-success"></div>
 			<small>Success</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch info"></div>
+			<div class="swatch background-info"></div>
 			<small>Info</small>
 		</div>
 	</div>
@@ -49,24 +49,80 @@
 	<h3 class="section-sub-title">Palette</h3>
 	<div class="row">
 		<div class="color column-1-6">
-			<div class="swatch light"></div>
+			<div class="swatch background-light"></div>
 			<small>Light</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch primary"></div>
+			<div class="swatch background-primary"></div>
 			<small>Primary</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch secondary"></div>
+			<div class="swatch background-secondary"></div>
 			<small>Secondary</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch tertiary"></div>
+			<div class="swatch background-tertiary"></div>
 			<small>Tertiary</small>
 		</div>
 		<div class="color column-1-6">
-			<div class="swatch dark"></div>
+			<div class="swatch background-dark"></div>
 			<small>Dark</small>
 		</div>
 	</div>
+	
+	<!-- Text Colors -->
+	<h3 class="section-sub-title">Text Colors</h3>
+	
+	<p class="text-color text-white background-grey">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-light-grey background-grey">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-grey">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-dark-grey">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-black">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	
+	<p class="text-color text-error">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-warning">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-success">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-info">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	
+	<p class="text-color text-light background-grey">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-primary">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-secondary">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-tertiary">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+	<p class="text-color text-dark">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+
+	<pre class="language-markup"><code class="language-markup"><?php echo htmlentities('<p class="text-white">...</p>
+<p class="text-light-grey">...</p>
+<p class="text-grey">...</p>
+<p class="text-dark-grey">...</p>
+<p class="text-black">...</p>
+<p class="text-error">...</p>
+<p class="text-warning">...</p>
+<p class="text-success">...</p>
+<p class="text-info">...</p>
+<p class="text-light">...</p>
+<p class="text-primary">...</p>
+<p class="text-secondary">...</p>
+<p class="text-tertiary">...</p>
+<p class="text-dark">...</p>'); ?>
+	</code></pre>
+	
+	<!-- Background Colors -->
+	<h3 class="section-sub-title">Background Colors</h3>
+
+	<pre class="language-markup"><code class="language-markup"><?php echo htmlentities('<div class="background-white">...</div>
+<div class="background-light-grey">...</div>
+<div class="background-grey">...</div>
+<div class="background-dark-grey">...</div>
+<div class="background-black">...</div>
+<div class="background-error">...</div>
+<div class="background-warning">...</div>
+<div class="background-success">...</div>
+<div class="background-info">...</div>
+<div class="background-light">...</div>
+<div class="background-primary">...</div>
+<div class="background-secondary">...</div>
+<div class="background-tertiary">...</div>
+<div class="background-dark">...</div>'); ?>
+	</code></pre>
+	
 </section>
