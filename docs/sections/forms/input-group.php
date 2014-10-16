ec082ff49316304ba63b04bb11f4dde7034a1469
Scott de Jonge <dejonge.scott@gmail.com>
2014-10-16 11:23:55 +1000

    Docs: Input Group Sizes

--- docs/sections/forms/input-group.php
+++ docs/sections/forms/input-group.php
@@ -1,56 +1,63 @@
-<section id="input-group" class="section">
+<?php
 
-<h2 class="section-title">Input Group<code>forms/input-group.less</code></h2>
-
-<h3 class="section-sub-title">Input Group</h3>
-<form>
-	<div class="input-group">
-		<input type="text" placeholder="Enter Keyword...">
-		<span class="input-group-button">
-			<button class="button button-primary" type="submit">Submit</button>
-		</span>
-	</div>
-</form>
-
-<pre class="language-markup"><code class="language-markup"><?php echo htmlentities('<div class="input-group">
+$inputs = array(
+array (
+	'title' => 'Input Group',
+	'code' => '<div class="input-group">
 	<input type="text" placeholder="Enter Keyword...">
 	<span class="input-group-button">
 		<button class="button button-primary" type="submit">Submit</button>
 	</span>
-</div>'); ?></code></pre>
-
-<h3 class="section-sub-title">Input Group Sizes</h3>
-<form role="form">
-	<div class="input-group">
-		<input class="search form-input input-large" type="text" placeholder="Input Group Large">
-		<span class="input-group-button input-group-button-large">
-			<button class="button button-primary" type="submit">Submit</button>
-		</span>
-	</div>
-</form>
-
-<pre class="language-markup"><code class="language-markup"><?php echo htmlentities('<div class="input-group">
-	<input class="search form-input input-large" type="text" placeholder="Enter Keyword...">
+</div>',
+),
+array (
+	'title' => 'Input Group Sizes',
+	'code' => '<div class="input-group">
+	<input class="input-extra-large" type="text" placeholder="Input Group Extra Large">
+	<span class="input-group-button input-group-button-extra-large">
+		<button class="button button-primary" type="submit">Submit</button>
+	</span>
+</div>
+<div class="input-group">
+	<input class="input-large" type="text" placeholder="Input Group Large">
 	<span class="input-group-button input-group-button-large">
 		<button class="button button-primary" type="submit">Submit</button>
 	</span>
-</div>'); ?></code></pre>
-
-<h3 class="section-sub-title">Search Form</h3>
-<form role="form">
-	<div class="input-group input-group-search">
-		<input type="search" placeholder="Search">
-		<span class="input-group-button">
-			<button class="button button-primary" type="submit"><span class="icon icon-search"></span></button>
-		</span>
-	</div>
-</form>
-
-<pre class="language-markup"><code class="language-markup"><?php echo htmlentities('<div class="input-group input-group-search">
-	<input type="search" placeholder="Enter Keyword...">
+</div>
+<div class="input-group">
+	<input class="input-medium" type="text" placeholder="Input Group Medium">
+	<span class="input-group-button input-group-button-medium">
+		<button class="button button-primary" type="submit">Submit</button>
+	</span>
+</div>
+<div class="input-group">
+	<input class="input-small" type="text" placeholder="Input Group Small">
+	<span class="input-group-button input-group-button-small">
+		<button class="button button-primary" type="submit">Submit</button>
+	</span>
+</div>',
+),
+array (
+'title' => 'Input Group Search',
+	'code' => '<div class="input-group input-group-search">
+	<input type="search" placeholder="Search">
 	<span class="input-group-button">
 		<button class="button button-primary" type="submit"><span class="icon icon-search"></span></button>
 	</span>
-</div>'); ?></code></pre>
+</div>',
+),
+);
+
+?>
+
+<section id="input-group" class="section">
+
+<h2 class="section-title">Input Group<code>forms/input-group.less</code></h2>
+
+<?php foreach ($inputs as $input) : ?>
+	<h3 class="section-sub-title"><?php echo $input['title']; ?></h3>
+	<form><?php echo $input['code']; ?></form>
+	<pre class="language-markup"><code class="language-markup"><?php echo htmlentities($input['code']); ?></code></pre>
+<?php endforeach; ?>
 
 </section>
