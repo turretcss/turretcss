a5a92ef3ff3f5740d07a10e61898dbaa494f8596
Scott de Jonge <dejonge.scott@gmail.com>
2015-01-23 14:32:33 +1000

    npm build/watch scripts. Moved libs and styles to vendor and styles folders.

--- docs/includes/head.php
+++ docs/includes/head.php
@@ -6,23 +6,10 @@
 	<title>Turret</title>
 	<meta name="description" content="A library of Documentation, Elements, Components, Icons, and a Grid for rapid responsive website development.">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<link rel="stylesheet" type="text/css" href="css/style.css" />
-
-	<!-- Syntax Higlighting -->
-	<link rel="stylesheet" href="lib/prism.css">
-	<script src="lib/prism.js"></script>
-	
-	<!-- Icons -->
 	<link rel="shortcut icon" href="/docs/ico/favicon.ico" type="image/x-icon" />
 	<link rel="icon" href="/docs/ico/favicon.ico" type="image/x-icon" />
-	<link rel="apple-touch-icon" href="/ico/AppIcon57x57.png" sizes="57x57" />
-	<link rel="apple-touch-icon" href="/ico/AppIcon72x72.png" sizes="72x72" />
-	<link rel="apple-touch-icon" href="/ico/AppIcon76x76.png" sizes="76x76" />
-	<link rel="apple-touch-icon" href="/ico/AppIcon57x57@2x.png" sizes="114x114" />
-	<link rel="apple-touch-icon" href="/ico/AppIcon60x60@2x.png" sizes="120x120" />
-	<link rel="apple-touch-icon" href="/ico/AppIcon72x72@2x.png" sizes="144x144" />
-	<link rel="apple-touch-icon" href="/ico/AppIcon76x76@2x.png" sizes="152x152" />
-	
-	
+	<link rel="stylesheet" href="vendor/prism.css">
+	<script src="vendor/prism.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/styles.css" />
 </head>
 <body>
