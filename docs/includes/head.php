120ff8af7dc7b1d2df89b4890ba87750fc13b3a3
Scott de Jonge <dejonge.scott@gmail.com>
2015-01-29 16:52:31 +1000

    Docs Google Analytics

--- docs/includes/head.php
+++ docs/includes/head.php
@@ -11,5 +11,14 @@
 	<link rel="stylesheet" href="/docs/vendor/prism.css">
 	<script src="/docs/vendor/prism.js"></script>
 	<link rel="stylesheet" type="text/css" href="/docs/css/styles.css" />
+	
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-59106857-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 </head>
 <body>
