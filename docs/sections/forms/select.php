5e2928dbae45f3d6d5bf50b1129249c380940e48
Scott de Jonge <dejonge.scott@gmail.com>
2015-01-30 15:27:49 +1000

    Select Sizes

--- docs/sections/forms/select.php
+++ docs/sections/forms/select.php
@@ -21,16 +21,6 @@ array (
 	</select>
 </label>',
 ),
-array (
-	'title' => 'Large Select Field',
-	'code' => '<label class="select select-large" for="select-large">
-	<select id="select-large">
-		<option>Select Field</option>
-		<option value="1">Option 01</option>
-		<option value="2">Option 02</option>
-	</select>
-</label>',
-),
 );
 ?>
 
@@ -41,4 +31,65 @@ array (
 		<form><?php echo $input['code']; ?></form>
 		<pre class="language-markup"><code class="language-markup"><?php echo htmlentities($input['code']); ?></code></pre>
 	<?php endforeach; ?>
+	
+	<h3 class="section-sub-title">Select Sizes</h3>
+	<form>
+		<label class="select select-extra-large" for="select-extra-large">
+			<select id="select-extra-large">
+				<option>Select Extra Large</option>
+				<option value="1">Option 01</option>
+				<option value="2">Option 02</option>
+			</select>
+		</label>
+		<label class="select select-large" for="select-large">
+			<select id="select-large">
+				<option>Select Large</option>
+				<option value="1">Option 01</option>
+				<option value="2">Option 02</option>
+			</select>
+		</label>
+		<label class="select select-medium" for="select-medium">
+			<select id="select-medium">
+				<option>Select Medium</option>
+				<option value="1">Option 01</option>
+				<option value="2">Option 02</option>
+			</select>
+		</label>
+		<label class="select select-small" for="select-small">
+			<select id="select-small">
+				<option>Select Small</option>
+				<option value="1">Option 01</option>
+				<option value="2">Option 02</option>
+			</select>
+		</label>
+	</form>
+	
+<pre class="language-markup"><code class="language-markup"><?php echo htmlentities('<label class="select select-extra-large" for="select-extra-large">
+	<select id="select-extra-large">
+		<option>Select Extra Large</option>
+		<option value="1">Option 01</option>
+		<option value="2">Option 02</option>
+	</select>
+</label>
+<label class="select select-large" for="select-large">
+	<select id="select-large">
+		<option>Select Large</option>
+		<option value="1">Option 01</option>
+		<option value="2">Option 02</option>
+	</select>
+</label>
+<label class="select select-medium" for="select-medium">
+	<select id="select-medium">
+		<option>Select Medium</option>
+		<option value="1">Option 01</option>
+		<option value="2">Option 02</option>
+	</select>
+</label>
+<label class="select select-small" for="select-small">
+	<select id="select-small">
+		<option>Select Small</option>
+		<option value="1">Option 01</option>
+		<option value="2">Option 02</option>
+	</select>
+</label>'); ?></code></pre>
 </section>
