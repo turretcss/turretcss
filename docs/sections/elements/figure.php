978b744cd8999808edc4451706fa3fc8430e65a5
Scott de Jonge <dejonge.scott@gmail.com>
2015-04-17 14:27:16 +1000

    Figure definitions in docs. Fixed figure font-family and font-weight definitions.

--- docs/sections/elements/figure.php
+++ docs/sections/elements/figure.php
@@ -12,6 +12,17 @@
 	</div>
 </div>
 
+<pre class="language-less"><code class="language-less"><?php echo htmlentities('@figure-margin: 0;
+@figure-padding: @gutter;
+@figure-border: 1px solid @light-grey;
+
+@figure-caption-margin: 10px 0px;
+@figure-caption-font-family: @paragraph-font-family;
+@figure-caption-font-weight: @paragraph-font-weight;
+@figure-caption-font-size: @paragraph-font-size;
+@figure-caption-color: @body-color;
+@figure-caption-text-align: center;'); ?></code></pre>
+
 <pre class="language-markup"><code class="language-markup"><?php echo htmlentities('<figure>
 	<div class="media media-1">
 		<img src="..." class="responsive">
