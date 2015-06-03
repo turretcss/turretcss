821416a98f1984815a2a8c84b44512fd0218c5f8
Scott de Jonge <dejonge.scott@gmail.com>
2015-06-03 12:04:29 +1000

    Docs: Minor styles. Scroll Spy fixes.

--- docs/js/spy.js
+++ docs/js/spy.js
@@ -10,9 +10,12 @@ $(document).ready(function () {
 
 			if (id.position().top <= top && id.position().top + id.height() > top) {
 				$(links).removeClass('active');
+				$(links).parent('li').removeClass('active');
 				current.addClass('active');
+				current.parent('li').addClass('active');
 			} else {
 				current.removeClass('active');
+				current.parent('li').removeClass('active');
 			}
 		});
 	});
