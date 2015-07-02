87e263e7b11848b63cebcdd8fa62f4e95f28df42
Scott de Jonge <dejonge.scott@gmail.com>
2015-07-02 16:39:32 +1000

    Commit config.php for docs.

---
+++ docs/config.php
@@ -0,0 +1,5 @@
+<?php
+	define('ROOT', 'http://dev.turret/');
+	define('DEFINITIONS', ROOT . '/docs/definitions/');
+	define('SAMPLES', ROOT . '/docs/samples/');
+	define('VERSION', `git describe --abbrev=0`);
