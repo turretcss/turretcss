1e09eb57f0f1882f35d633143fff771de3f335a0
Scott de Jonge <dejonge.scott@gmail.com>
2015-10-23 16:07:10 +1000

    Removed PHP docs

--- docs/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-	define('ROOT', 'http://dev.turret/');
-	define('DEFINITIONS', ROOT . '/docs/definitions/');
-	define('SAMPLES', ROOT . '/docs/samples/');
-	define('VERSION', trim(`git describe --abbrev=0`));
