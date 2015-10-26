779e86270639e226800257c3ebfe78406ffd9b48
Scott de Jonge <dejonge.scott@gmail.com>
2015-10-26 17:23:01 +1000

    Added main.js with watch and build scripts

---
+++ docs/assets/scripts/main.js
@@ -0,0 +1,49 @@
+/*
+ * Helper Functions
+ */
+
+var hasClass = function (elem, className) {
+  return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+var addClass = function (elem, className) {
+  if (!hasClass(elem, className)) {
+    elem.className += ' ' + className;
+  }
+}
+
+var removeClass = function (elem, className) {
+  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+      newClass = newClass.replace(' ' + className + ' ', ' ');
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+  }
+}
+
+
+/*
+ * Toggle
+ */
+
+var toggle = document.getElementById('toggle');
+var main = document.querySelector('.docs-main');
+var body = document.querySelector('body');
+
+toggle.addEventListener('click', function(event) {
+  event.preventDefault();
+  body.classList.toggle('open');
+});
+
+main.addEventListener('click', function(event) {
+
+  if (event.target != toggle) {
+    event.preventDefault();
+
+    if (hasClass(body, 'open')) {
+      removeClass(body, 'open');
+    }
+  }
+});
