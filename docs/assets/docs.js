9d0e55e4dd6c993a4164b1d3c4548aa50c850e78
Scott de Jonge <dejonge.scott@gmail.com>
2016-11-25 16:30:37 +1000

    Toggle class

--- docs/assets/docs.js
+++ docs/assets/docs.js
@@ -1,1 +1,13 @@
 require('./css/main.css');
+
+var toggle = document.getElementById('toggle');
+var body = document.getElementsByTagName('body')[0];
+
+toggle.addEventListener('click', function () {
+	
+	if (body.classList.contains('open')) {
+		body.classList.remove('open');
+	} else {
+		body.classList.add('open');
+	}
+});
